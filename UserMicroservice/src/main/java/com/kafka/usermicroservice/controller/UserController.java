79535cf570fa6623858140652de1b57469890809
Oleh Karmazin <kalemzwork@gmail.com>
2025-08-10 18:11:06 +0200

    feat: implemented OAuth2 with Keycloak

---
+++ UserMicroservice/src/main/java/com/kafka/usermicroservice/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.kafka.usermicroservice.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @GetMapping
+    @PreAuthorize("hasRole('user')")
+    public String test(){
+        return "hello user!";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('admin')")
+    public String test_admin(){
+        return "hello Admin!!!";
+    }
+}
